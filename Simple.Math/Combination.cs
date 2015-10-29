67f4a68e090b0afdbabbef003a1bb166d74a38c5
Lukas Sedlacek <lsedlacek@inbox.com>
2015-10-29 15:49:48 +0000

    Combinations and Sets

---
+++ Simple.Math/Combination.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Simple.Math
+{
+    public static class Combination
+    {
+        /// <summary>
+        /// Refer to <a href="http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.205.2921&rep=rep1&type=pdf">the original</a>
+        /// for the smarts, I just implemented it in C#.
+        /// </summary>
+        /// <returns>All distinct ulongs with <paramref name="k"/> positive bits in the <paramref name="n"/> bit space.</returns>
+        public static IEnumerable<ulong> GetAll(int n, int k)
+        {
+            if (n < 1 || n > 63) throw new ArgumentOutOfRangeException(nameof(n), $"{n} is not within range [1, 63].");
+            if (k < 0 || k > n) throw new ArgumentOutOfRangeException(nameof(k), $"{k} it not withing range [0, {n}]");
+
+            if (k == 0)
+            {
+                yield return 0;
+                yield break;
+            }
+
+            var capBit = (1UL << n);
+
+            var result = (1UL << k) - 1UL;
+            do
+            {
+                yield return result;
+
+                var x = result & (result + 1UL);
+                var y = x ^ (x - 1UL);
+                x = y + 1UL;
+                y = y & result;
+                var z = x & result;
+                x = (z == 0UL) ? z : z - 1UL;
+                result = result + y - x;
+            } while ((result & capBit) != capBit);
+        }
+    }
+}
---
+++ Simple.Math/TinySet.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Simple.Math
+{
+    public struct TinySet : IEnumerable<int>
+    {
+        public const int Minimum = 0;
+        public const int Maximum = sizeof(ulong) * 8 - 1;
+
+        public static TinySet Empty { get; } = default(TinySet);
+
+        public TinySet(ulong bits)
+        {
+            Bits = bits;
+        }
+
+        public TinySet Add(int value)
+        {
+            CheckWhetherInRange(value);
+            return new TinySet(Bits | (1UL << value));
+        }
+
+        public TinySet Remove(int value)
+        {
+            CheckWhetherInRange(value);
+            return new TinySet(Bits ^ (1UL << value));
+        }
+
+        public bool Contains(int value)
+        {
+            CheckWhetherInRange(value);
+            var bit = 1UL << value;
+            return (Bits & bit) == bit;
+        }
+
+        private static void CheckWhetherInRange(int bit)
+        {
+            if (bit < Minimum || bit > Maximum)
+            {
+                throw new ArgumentOutOfRangeException(nameof(bit), $"{bit} is not in range [{Minimum}, {Maximum}].");
+            }
+        }
+
+        private ulong Bits { get; }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            var set = this;
+
+            return Enumerable.Range(Minimum, Maximum + 1)
+                .Where(value => set.Contains(value))
+                .GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
