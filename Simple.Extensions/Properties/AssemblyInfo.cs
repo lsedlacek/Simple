578510de0df181029b7cd494fe8b51903d3309b0
Lukas Sedlacek <lsedlacek@inbox.com>
2015-10-29 10:44:28 +0000

    EnumExtensions.HasFlags added

---
+++ Simple.Extensions/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Simple.Extensions")]
+[assembly: AssemblyProduct("Simple.Extensions")]
+[assembly: AssemblyCopyright("Copyright © lsedlacek 2015")]
+[assembly: ComVisible(false)]
+[assembly: Guid("C2223D5C-9EA3-45EC-890F-4488106B5FB1")]
